d2bded7a4730b1d035b5a72889d5d7e082c31d1f
Em Karisch <emii.khaos@gmail.com>
2026-08-06 17:34:45 +0200

    feat: add silicon mod

--- .dev/satisfactory-modeler.ps1
+++ .dev/satisfactory-modeler.ps1
@@ -1,4 +1,6 @@
 Copy-Item "${Env:SATISFACTORY_WORKSPACE}\KhaosSatisfactoryModelerData\game_data\game_data.json" -Destination "${Env:SATISFACTORY_WORKSPACE}\SatisfactoryModeler\game_data\game_data.json" -Force
-Copy-Item "${Env:SATISFACTORY_WORKSPACE}\KhaosSatisfactoryModelerData\images\icons" -Destination "${Env:SATISFACTORY_WORKSPACE}\SatisfactoryModeler\images\icons" -Recurse -Force
+Copy-Item "${Env:SATISFACTORY_WORKSPACE}\KhaosSatisfactoryModelerData\images\icons" -Destination "${Env:SATISFACTORY_WORKSPACE}\SatisfactoryModeler\images\" -Recurse -Force
 
+Set-Location "${Env:SATISFACTORY_WORKSPACE}\SatisfactoryModeler"
 Start-Process -FilePath "${Env:SATISFACTORY_WORKSPACE}\SatisfactoryModeler\satisfactory_modeler.exe"
+Set-Location "${Env:SATISFACTORY_WORKSPACE}\KhaosSatisfactoryModelerData"
